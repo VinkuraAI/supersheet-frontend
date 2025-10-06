d83e91c76065fcaef4bf781afaab1e69ec5acc87
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-10-06 09:27:45 +0530

    refactor(next.config): Enable ESLint error ignoring during production builds

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  experimental: {
+    serverComponentsExternalPackages: ["@acme/ui"],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://supersheet-backend.onrender.com/:path*",
+      },
+    ];
   },
 };
 
