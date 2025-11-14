7cb663dc3340b6626339e8ef16c675b2f51c855e
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-11-14 18:48:15 +0530

    feat(config): add API rewrites for local development

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/:path*",
+      },
+    ];
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
